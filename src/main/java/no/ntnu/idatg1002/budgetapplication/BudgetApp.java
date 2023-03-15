dd0ce0bab95c81db5dff78788518131c34192c7e
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-03-15 17:45:05 +0100

    Added JavaDoc to Database class and moved Account to package

--- src/main/java/no/ntnu/idatg1002/budgetapplication/BudgetApp.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/BudgetApp.java
@@ -2,6 +2,7 @@ package no.ntnu.idatg1002.budgetapplication;
 
 import no.ntnu.idatg1002.budgetapplication.backend.accountinformation.Account;
 import no.ntnu.idatg1002.budgetapplication.backend.SecurityQuestion;
+import no.ntnu.idatg1002.budgetapplication.backend.accountinformation.Database;
 
 /**
  * The main class of application.
@@ -10,11 +11,15 @@ public class BudgetApp {
 
   public static void main(String[] args) {
     //Code here
-    Account account = new Account("Simon", "s@com", "1234",
+    Account account1 = new Account("Simon", "s@com", "1234",
         SecurityQuestion.CAR_BRAND, "BMW");
-    System.out.println(account.getAccountNumber());
-    for (SecurityQuestion q : SecurityQuestion.values()) {
-      System.out.println(q.getSecurityQuestionString());
+    Account account2 = new Account("Emil", "emilgmail.com", "12341",
+        SecurityQuestion.FAVORITE_FOOD, "Pizza");
+    Database.addAccount(account1);
+    Database.addAccount(account2);
+
+    for (Account a : Database.getAccounts().values()) {
+      System.out.println(a);
     }
   }
 
