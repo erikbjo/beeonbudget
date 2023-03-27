5d1a6c174fa5ed6d7fb9606b23e36106bf072d29
Emil <emilkle@stud.ntnu.no>
2023-03-27 16:49:11 +0200

    Added a image folder to resources and added some small changes to primary.fxml

--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/PrimaryView.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/PrimaryView.java
@@ -1,26 +1,27 @@
 package no.ntnu.idatg1002.budgetapplication.frontend;
 
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class BudgetView extends Application {
+public class PrimaryView extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    /**
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/primary.fxml"));
     Parent root = loader.load();
     Scene scene = new Scene(root);
+    primaryStage.getIcons().add(new Image(
+        getClass().getResource("/images/simpleLogoBoY.png").openStream()));
+    //primaryStage.getIcons().add(image);
     primaryStage.setScene(scene);
     primaryStage.show();
-     */
   }
 
-  public static void budgetView(String[] args) {
+  public static void mainApp(String[] args) {
     launch();
   }
 }
