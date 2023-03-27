16292da4e872c25dad033bceacd186601fd7b688
Emil <emilkle@stud.ntnu.no>
2023-03-27 17:19:28 +0200

    Created PrimaryController class and added some small changes to the .fxml file

--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/PrimaryView.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/PrimaryView.java
@@ -14,9 +14,9 @@ public class PrimaryView extends Application {
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/primary.fxml"));
     Parent root = loader.load();
     Scene scene = new Scene(root);
+    primaryStage.setTitle("Bee on Budget");
     primaryStage.getIcons().add(new Image(
         getClass().getResource("/images/simpleLogoBoY.png").openStream()));
-    //primaryStage.getIcons().add(image);
     primaryStage.setScene(scene);
     primaryStage.show();
   }
