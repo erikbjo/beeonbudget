e3513d734335561d944673e8df98af12f2e17733
Emil <emilkle@stud.ntnu.no>
2023-03-27 16:49:11 +0200

    Added a image folder to resources and added some small changes to primary.fxml

--- src/main/java/no/ntnu/idatg1002/budgetapplication/Main.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/Main.java
@@ -1,11 +1,12 @@
 package no.ntnu.idatg1002.budgetapplication;
 
 import no.ntnu.idatg1002.budgetapplication.frontend.BudgetView;
+import no.ntnu.idatg1002.budgetapplication.frontend.PrimaryView;
 
 public class Main {
 
   public static void main(String[] args) {
-    BudgetView.mainApp(args);
+    PrimaryView.mainApp(args);
   }
 
 }
