6bbd648d7536e9489772c784539eed1e554cdf1a
Eskil Alstad <eskilal@stud.ntnu.no>
2023-03-28 10:36:29 +0200

    added methods for controllers

--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/PrimaryController.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/PrimaryController.java
@@ -1,4 +1,32 @@
 package no.ntnu.idatg1002.budgetapplication.frontend.controller;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class PrimaryController {
+
+  private Stage stage;
+  private Scene scene;
+  private Parent parent;
+
+  public void switchToBudget(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("budget.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void switchToPrimary(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 }
