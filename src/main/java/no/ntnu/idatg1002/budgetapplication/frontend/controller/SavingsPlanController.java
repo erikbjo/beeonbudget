f124fcd90fae8e4f019589469a5bb59f340c562e
Eskil Alstad <eskilal@stud.ntnu.no>
2023-03-28 10:21:10 +0200

    refactored folders for frontend and added paths for controllers

---
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/SavingsPlanController.java
@@ -0,0 +1,3 @@
+package no.ntnu.idatg1002.budgetapplication.frontend.controller;
+
+public class SavingsPlanController {}
--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/view/SavingsPlanView.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/view/SavingsPlanView.java
@@ -1,4 +1,4 @@
-package no.ntnu.idatg1002.budgetapplication.frontend;
+package no.ntnu.idatg1002.budgetapplication.frontend.view;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
