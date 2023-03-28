ad9d961a29689861fd100079d1955cad84fab915
dzudaka <igormd@ntnu.no>
2023-03-28 16:57:59 +0200

    WIP GUI

--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/SavingsPlanController.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/SavingsPlanController.java
@@ -2,17 +2,25 @@ package no.ntnu.idatg1002.budgetapplication.frontend.controller;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.*;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.stage.Popup;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import no.ntnu.idatg1002.budgetapplication.backend.savings.SavingsPlan;
 
 public class SavingsPlanController {
 
   private Stage stage;
   private Scene scene;
   private Parent parent;
+  private Popup popup;
+
+
+
   public void switchToPrimaryFromSavingPlan(ActionEvent event) throws IOException {
     Parent root = FXMLLoader.load(getClass().getResource("/fxmlfiles/primary.fxml"));
     stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -20,4 +28,22 @@ public class SavingsPlanController {
     stage.setScene(scene);
     stage.show();
   }
+
+  public void examplePlan(){
+    SavingsPlan examplePlan = new SavingsPlan("SavingsPlan1", 100, 1);
+    examplePlan.setWantedSavingTime(1);
+    examplePlan.setWantedMonthlySavingAmount(1);
+  }
+
+  public void onNewSavingsPlan(){
+    Popup popup = new Popup();
+
+  }
+
+  public void onEstimateMonths(){
+
+  }
+
+
+
 }
