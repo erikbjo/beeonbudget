e201bf50be04dd3d3df6f67862a97bea3e29710c
Eskil Alstad <eskilal@stud.ntnu.no>
2023-03-28 10:53:14 +0200

    Conflickt

---
+++ src/test/java/no/ntnu/idatg1002/budgetapplication/backend/savings/SavingsPlanTest.java
@@ -0,0 +1,29 @@
+package no.ntnu.idatg1002.budgetapplication.backend.savings;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SavingsPlanTest {
+
+  SavingsPlan savingsPlan;
+
+  @BeforeEach
+  void setUp() {
+    savingsPlan = new SavingsPlan("Test", 200, 100);
+  }
+
+  @Test
+  void goalNameEqualTrue(){
+    
+  }
+
+  @Test
+  void goalNameEqualFalse(){
+
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+}
