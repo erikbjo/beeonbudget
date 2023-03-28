6be96c435698674f953be713084162207838d169
Eskil Alstad <eskilal@stud.ntnu.no>
2023-03-28 10:17:42 +0200

    added import classes

--- src/test/java/no/ntnu/idatg1002/budgetapplication/backend/BudgetTest.java
+++ src/test/java/no/ntnu/idatg1002/budgetapplication/backend/BudgetTest.java
@@ -2,64 +2,53 @@ package no.ntnu.idatg1002.budgetapplication.backend;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BudgetTest {
+class BudgetTest
+{
 
-  private Budget budget;
-  private Income income;
-  private Expense expense;
+    private Budget budget;
+    private Income income;
+    private Expense expense;
 
-  @BeforeEach
-  void setUp() {
-    budget = new Budget("Test");
-    income = new Income(200, "Test income 1", Category.HOUSING, RecurringType.NONRECURRING);
-    expense = new Expense(300, "Test expense 1", Category.HOUSING, RecurringType.NONRECURRING);
-    budget.addBudgetIncome(income);
-    budget.addBudgetExpenses(expense);
-
-    // Adding all categories to the budget
-    for (Category cat : Category.values()) {
-      budget.addCategory(cat);
+    @BeforeEach
+    void setUp()
+    {
+        budget = new Budget("Test");
+        income = new Income(200, "sdvdvdvs", Category.HOUSING,
+            RecurringType.NONRECURRING);
+        expense = new Expense(300, "eseglerl", Category.HOUSING,
+            RecurringType.NONRECURRING);
+        budget.addBudgetIncome(income);
+        budget.addBudgetExpenses(expense);
     }
-  }
 
-  @Test
-  void getNetBalanceSmallerThanZero() {
-    int netBalance = budget.getNetBalance();
+    @Test
+    void getNetBalanceSmallerThanZero()
+    {
+        int netBalance = budget.getNetBalance();
 
-    assertTrue(netBalance < 0);
-  }
+        assertTrue(netBalance < 0);
+    }
 
-  @Test
-  void getNetBalanceEqualToFifty() {
-    budget.getIncomeList().remove(income);
-    budget.getExpenseList().remove(expense);
-    budget.addBudgetIncome(
-        new Income(200, "Test income 2", Category.HOUSING, RecurringType.NONRECURRING));
-    budget.addBudgetExpenses(
-        new Expense(150, "Test expense 2", Category.HEALTHCARE, RecurringType.MONTHLY));
+    @Test
+    void getNetBalanceEqualToFifty()
+    {
+        budget.getIncomeList().remove(income);
+        budget.getExpenseList().remove(expense);
+        budget.addBudgetIncome(new Income(200, "jbfgdfgui", Category.HOUSING,
+            RecurringType.NONRECURRING));
+        budget.addBudgetExpenses(new Expense(150, "JVhfyhkguug",
+            Category.HEALTHCARE, RecurringType.MONTHLY));
 
-    int netBalance = budget.getNetBalance();
-    assertEquals(50, netBalance);
-  }
+        int netBalance = budget.getNetBalance();
+        assertEquals(50, netBalance);
+    }
 
-  @Test
-  void getCategoryListReturnsEnum() {
-    assertTrue(budget.getCategoryList().containsAll(List.of(Category.values())));
-  }
-
-  @Test
-  void setBudgetNameNeedsToHaveValidName() {
-    budget.setBudgetName("Control name");
-    budget.setBudgetName("");
-    budget.setBudgetName(" ");
-    assertEquals("Control name", budget.getBudgetName());
-  }
-
-  @AfterEach
-  void tearDown() {}
+    @AfterEach
+    void tearDown()
+    {
+    }
 }
