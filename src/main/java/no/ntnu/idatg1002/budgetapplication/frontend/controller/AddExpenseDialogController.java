cf72087d3f1744f1188dc8718278622afe70e00f
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-03-29 11:14:33 +0200

    added fields for fxml

--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/AddExpenseDialogController.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/AddExpenseDialogController.java
@@ -2,9 +2,22 @@ package no.ntnu.idatg1002.budgetapplication.frontend.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Dialog;import no.ntnu.idatg1002.budgetapplication.backend.Budget;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
+import no.ntnu.idatg1002.budgetapplication.backend.Budget;
+import no.ntnu.idatg1002.budgetapplication.backend.Category;
+import no.ntnu.idatg1002.budgetapplication.backend.RecurringType;
 
 public class AddExpenseDialogController extends Dialog<Budget> {
+
+  @FXML private ComboBox<Category> categoryComboBox;
+
+  @FXML private TextField expenseAmount;
+
+  @FXML private TextField expenseDescription;
+
+  @FXML private ComboBox<RecurringType> recurringIntervalComboBox;
 
   @FXML
   void onAddExpense(ActionEvent event) {}
--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/AddIncomeDialogController.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/controller/AddIncomeDialogController.java
@@ -2,10 +2,24 @@ package no.ntnu.idatg1002.budgetapplication.frontend.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
 import no.ntnu.idatg1002.budgetapplication.backend.Budget;
+import no.ntnu.idatg1002.budgetapplication.backend.RecurringType;
 
 public class AddIncomeDialogController extends Dialog<Budget> {
+
+  @FXML private Button addIncomeButton;
+
+  @FXML private Button cancelButton;
+
+  @FXML private TextField incomeAmount;
+
+  @FXML private TextField incomeDescription;
+
+  @FXML private ComboBox<RecurringType> recurringIntervalComboBox;
 
   @FXML
   void onAddIncome(ActionEvent event) {}
