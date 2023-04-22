8af6766ddebd5c203bb169245f3c869f095c18b3
Erik Bjørnsen <erbj@stud.ntnu.no>
2023-04-22 13:31:51 +0200

    removed useless constructurs

--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/alerts/ConfirmationAlert.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/alerts/ConfirmationAlert.java
@@ -1,18 +1,9 @@
 package no.ntnu.idatg1002.budgetapplication.frontend.alerts;
 
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 
 public class ConfirmationAlert extends Alert {
-  private ConfirmationAlert(AlertType alertType) {
-    super(alertType);
-  }
-
-  private ConfirmationAlert(AlertType alertType, String s, ButtonType... buttonTypes) {
-    super(alertType, s, buttonTypes);
-  }
-
   public ConfirmationAlert(String title, String contentText) {
     super(AlertType.CONFIRMATION);
     this.initModality(Modality.APPLICATION_MODAL);
--- src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/alerts/ExceptionAlert.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/frontend/alerts/ExceptionAlert.java
@@ -1,19 +1,9 @@
 package no.ntnu.idatg1002.budgetapplication.frontend.alerts;
 
-import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 
 public class ExceptionAlert extends Alert {
-  private ExceptionAlert(AlertType alertType) {
-    super(alertType);
-  }
-
-  private ExceptionAlert(AlertType alertType, String s, ButtonType... buttonTypes) {
-    super(alertType, s, buttonTypes);
-  }
-
   public ExceptionAlert(Exception exception) {
     super(Alert.AlertType.WARNING);
     this.setTitle("Error");
