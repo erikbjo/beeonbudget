ae77e8e3850b2e68f67fba2e8c99e43480cbffae
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-04-22 13:07:01 +0200

    Removed AccountList and changed AccountListInterface to a DAO interface

--- src/main/java/no/ntnu/idatg1002/budgetapplication/backend/accountinformation/DAO.java
+++ src/main/java/no/ntnu/idatg1002/budgetapplication/backend/accountinformation/DAO.java
@@ -2,7 +2,7 @@ package no.ntnu.idatg1002.budgetapplication.backend.accountinformation;
 
 import java.util.Iterator;
 
-public interface AccountListInterface extends Iterable<Account> {
+public interface DAO extends Iterable<Account> {
   void addAccount(Account account);
 
   void removeAccount(Account account);
